918cbe666323637533246fef5e73e6fdbffbe796
Rudy <rudydimacali@gmail.com>
2019-03-05 10:47:57 -0800

    Initialize with Chapter 1 Problems.

---
+++ Chapter 1/isUnique.js
@@ -0,0 +1,6 @@
+/*
+
+Implement an algorithm to determine if a string has all unique characters.What if you
+cannot use additional data structures ?
+
+*/
---
+++ Chapter 1/zeroMatrix.js
@@ -0,0 +1,6 @@
+/*
+
+Write an algorithm such that if an element in an MxN matrix is 0, its entire row and
+column are set to O.
+
+*/
