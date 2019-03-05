756ad532145d141186b31a9efeb4eb4892b9ffc8
Rudy <rudydimacali@gmail.com>
2019-03-05 10:59:42 -0800

    Complete checkPermutation.js.

--- Chapter 1 - Arrays & Strings/checkPermutation.js
+++ Chapter 1 - Arrays & Strings/checkPermutation.js
@@ -5,3 +5,23 @@ other.
 
 */
 
+const assert = require('assert');
+
+const checkPermutation = (strOne, strTwo) => {
+  if (strOne.length !== strTwo.length) return false;
+  const letters = {};
+  for (let i = 0; i < strOne.length; i += 1) {
+    letters[strOne[i]] = letters[strOne[i]] === undefined ? 1 : letters[strOne[i]] + 1;
+  }
+  for (let j = 0; j < strTwo.length; j += 1) {
+    if (letters[strTwo[j]] === undefined) return false;
+    letters[strTwo[j]] -= 1;
+    if (letters[strTwo[j]] < 0) return false;
+  }
+  return true;
+};
+
+assert.equal(checkPermutation('asd', 'das'), true);
+assert.equal(checkPermutation('jumping', 'ginpumj'), true);
+assert.equal(checkPermutation('testing', 'test'), false);
+assert.equal(checkPermutation('jump', 'gump'), false);
