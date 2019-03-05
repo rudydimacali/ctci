59441b37aeefd7b8422336217d7530e879213800
Rudy <rudydimacali@gmail.com>
2019-03-05 10:55:26 -0800

    Complete isUnique.js.

--- Chapter 1 - Arrays & Strings/isUnique.js
+++ Chapter 1 - Arrays & Strings/isUnique.js
@@ -1,6 +1,20 @@
 /*
 
-Implement an algorithm to determine if a string has all unique characters.What if you
-cannot use additional data structures ?
+Implement an algorithm to determine if a string has all unique characters.
+What if you cannot use additional data structures ?
 
 */
+
+const assert = require('assert');
+
+const isUnique = (string) => {
+  const letters = {};
+  for (let i = 0; i < string.length; i += 1) {
+    if (letters[string[i]] !== undefined) return false;
+    letters[string[i]] = 1;
+  }
+  return true;
+};
+
+assert.equal(isUnique('uniq'), true);
+assert.equal(isUnique('test'), false);
